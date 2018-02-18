5a7052aed76873f86d2df03141b9799d875380ba
Ivo de Concini <ivo.deconcini@zuehlke.com>
2018-02-18 17:49:42 +0100

    Cleanup post actions

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,9 +1,6 @@
 import {fetchPosts} from '../utils/serverApi'
 
 export const LOAD_POSTS = 'LOAD_POSTS';
-export const ADD_POST = 'ADD_POST';
-export const REMOVE_POST = 'REMOVE_POST';
-export const EDIT_POST = 'EDIT_POST';
 
 export function loadPosts(category) {
     return (dispatch) => {
@@ -13,26 +10,5 @@ export function loadPosts(category) {
                 posts,
                 category
             }))
-    }
-}
-
-export function addPost(post) {
-    return {
-        type: ADD_POST,
-        post
-    }
-}
-
-export function removePost(postId) {
-    return {
-        type: REMOVE_POST,
-        postId
-    }
-}
-
-export function editPost(post) {
-    return {
-        type: EDIT_POST,
-        post
     }
 }
